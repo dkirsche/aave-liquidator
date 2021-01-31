1fe34b2c968b02f8268700582e17ffcb33b4cd4b
Daniel Kirsche <dkirsche@Daniels-MacBook-Pro.local>
2021-01-31 06:33:55 -0600

    first commit

---
+++ contracts/BatchFlashDemo.sol
@@ -0,0 +1,176 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity 0.6.12;
+
+import { FlashLoanReceiverBase } from "FlashLoanReceiverBase.sol";
+import { ILendingPool, ILendingPoolAddressesProvider, IERC20 } from "Interfaces.sol";
+import { SafeMath } from "Libraries.sol";
+import "./Ownable.sol";
+
+import "https://github.com/sushiswap/sushiswap/blob/master/contracts/uniswapv2/interfaces/IUniswapV2Router02.sol";
+
+/*
+* A contract that executes the following logic in a single atomic transaction:
+*
+*   1. Gets a batch flash loan of AAVE, DAI and LINK
+*   2. Deposits all of this flash liquidity onto the Aave V2 lending pool
+*   3. Borrows 100 LINK based on the deposited collateral
+*   4. Repays 100 LINK and unlocks the deposited collateral
+*   5. Withdrawls all of the deposited collateral (AAVE/DAI/LINK)
+*   6. Repays batch flash loan including the 9bps fee
+*
+*/
+contract BatchFlashDemo is FlashLoanReceiverBase, Ownable {
+
+    ILendingPoolAddressesProvider provider;
+    IUniswapV2Router02 uniswapV2Router;
+    using SafeMath for uint256;
+
+    address lendingPoolAddr;
+
+
+    event ErrorHandled(string stringFailure);
+
+    // intantiate lending pool addresses provider and get lending pool address
+    constructor(ILendingPoolAddressesProvider _addressProvider, IUniswapV2Router02 _uniswapV2Router) FlashLoanReceiverBase(_addressProvider) public {
+        provider = _addressProvider;
+        lendingPoolAddr = provider.getLendingPool();
+
+        // instantiate UniswapV2 Router02
+        uniswapV2Router = IUniswapV2Router02(address(_uniswapV2Router));
+
+    }
+
+    /**
+        This function is called after your contract has received the flash loaned amount
+     */
+    function executeOperation(
+        address[] calldata assets,
+        uint256[] calldata amounts,
+        uint256[] calldata premiums,
+        address initiator,
+        bytes calldata params
+    )
+        external
+        override
+        returns (bool)
+    {
+
+        //collateral  the address of the token that we will be compensated in
+        //userToLiquidate - id of the user to liquidate
+        //amountOutMin - minimum amount of asset paid when swapping collateral
+
+        (address collateral, address userToLiquidate, uint256 amountOutMin, address[] memory swapPath) = abi.decode(params, (address, address, uint256, address[]));
+
+        //liquidate unhealthy loan
+        liquidateLoan(collateral, assets[0], userToLiquidate, amounts[0], false);
+
+        //swap collateral from liquidate back to asset from flashloan to pay it off
+        swapToBarrowedAsset(collateral,assets[0],amountOutMin, swapPath);
+
+        //Pay to owner the balance after fees
+        uint256 profit = calcProfits(IERC20(assets[0]).balanceOf(address(this)),amounts[0],premiums[0]);
+
+        require(profit > 0 , "No profit");
+        IERC20(assets[0]).transfer(owner(), profit);
+
+
+        // Approve the LendingPool contract allowance to *pull* the owed amount
+        // i.e. AAVE V2's way of repaying the flash loan
+            uint amountOwing = amounts[0].add(premiums[0]);
+            IERC20(assets[0]).approve(address(_lendingPool), amountOwing);
+
+        return true;
+    }
+
+    //calculate profits after paying back loan & fees
+    function calcProfits(uint256 _balance, uint256 _loanAmount, uint256 _loanFee)
+        pure
+        private
+        returns(uint256)
+    {
+        return _balance.sub(_loanAmount.add(_loanFee),"no profits to return");
+    }
+
+    function liquidateLoan(address _collateral, address _liquidate_asset, address _userToLiquidate, uint256 _amount, bool _receiveaToken) public {
+
+        require(IERC20(_liquidate_asset).approve(address(_lendingPool), _amount), "Approval error");
+
+        _lendingPool.liquidationCall(_collateral,_liquidate_asset, _userToLiquidate, _amount, _receiveaToken);
+    }
+
+
+    //assumes the balance of the token is on the contract
+    function swapToBarrowedAsset(address asset_from, address asset_to, uint amountOutMin, address[] memory swapPath ) public {
+        //todo - verify these direct paths are available in uniswap. might need to make this a param that is passed in by bot
+        //todo - handle eth swaps differently. need to check if eth or weth is used with liquidate.
+        IERC20 asset_fromToken;
+        uint256 amountToTrade;
+        uint deadline;
+
+        // setting deadline to avoid scenario where miners hang onto it and execute at a more profitable time
+        deadline = block.timestamp + 300; // 5 minutes
+
+        asset_fromToken = IERC20(asset_from);
+        amountToTrade = asset_fromToken.balanceOf(address(this));
+
+        // grant uniswap access to your token
+        asset_fromToken.approve(address(uniswapV2Router), amountToTrade);
+
+        // Trade 1: Execute swap from asset_from into designated ERC20 (asset_to) token on UniswapV2
+        try uniswapV2Router.swapExactTokensForTokens(
+            amountToTrade,
+            amountOutMin,
+            swapPath,
+            address(this),
+            deadline
+        ){
+        }
+        catch Error(string memory reason)
+        {
+            emit ErrorHandled(reason);
+        }
+        catch
+        {
+
+        }
+
+    }
+
+
+    /*
+    * This function is manually called to commence the flash loans sequence
+    */
+    function executeFlashLoans(address _assetToLiquidate, uint256 _flashAmt, address _collateral, address _userToLiquidate, uint256 _amountOutMin, address[] memory _swapPath) public onlyOwner {
+        address receiverAddress = address(this);
+
+        // the various assets to be flashed
+        address[] memory assets = new address[](1);
+        assets[0] = _assetToLiquidate;
+
+        // the amount to be flashed for each asset
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = _flashAmt;
+
+        // 0 = no debt, 1 = stable, 2 = variable
+        uint256[] memory modes = new uint256[](1);
+        modes[0] = 0;
+
+        address onBehalfOf = address(this);
+        //only for testing. must remove
+        
+
+        bytes memory params = abi.encode(_collateral, _userToLiquidate, _amountOutMin, _swapPath);
+        uint16 referralCode = 0;
+
+        _lendingPool.flashLoan(
+            receiverAddress,
+            assets,
+            amounts,
+            modes,
+            onBehalfOf,
+            params,
+            referralCode
+        );
+    }
+
+}
--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
+import { ChainId, Token, WETH, Fetcher, Route } from '@uniswap/sdk'
 import { setGlobals } from './globals';
-import { liquidate } from './liquidation/liquidation'
+import { liquidate } from './liquidation/liquidation';
+import { fetchV2UnhealthyLoans } from './v2liquidation';
+require('isomorphic-fetch');
 
 require('dotenv').config()
 
 setGlobals();
 
-const collateralAddress = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" 
+const collateralAddress = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
 const reserveAddress = "0xb36938c51c4f67e5e1112eb11916ed70a772bd75"
-const userLiquidated = "0x922257aefb9d47bfe36e7d72288c2cfb56457a40" 
+const userLiquidated = "0x922257aefb9d47bfe36e7d72288c2cfb56457a40"
 const purchaseAmount = '10'
 const receiveATokens = false
 
+//Uniswap constants
+const USDC_MAINNET = new Token(ChainId.MAINNET, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 6)
+const DAI_MAINNET = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18)
+const USDC_KOVAN = new Token(ChainId.KOVAN, '0xe22da380ee6B445bb8273C81944ADEB6E8450422', 6)
+const DAI_KOVAN = new Token(ChainId.KOVAN, '0xff795577d9ac8bd7d90ee22b6c1703490b6512fd', 18)
+var DAI_WETH;
+//-----
+/*
 liquidate(
     collateralAddress,
     reserveAddress,
@@ -18,3 +29,112 @@ liquidate(
     purchaseAmount,
     receiveATokens
 )
+*/
+delayedFetchUnhealthyLoans();
+
+//infinite loop calling fetchUnhealthyLoans
+//sleep for 1 minute before each call
+async function delayedFetchUnhealthyLoans(){
+  while(1==1){
+    console.log("fetching loans")
+    fetchV2UnhealthyLoans();
+    updateSwapPrices();
+    await sleep(60000);
+  }
+}
+//fetch all users that have borrowed funds
+//calculate HealthFactor
+//output all users with Loans that can be liquidated
+async function fetchUnhealthyLoans(){
+var count=0;
+while(count<6){
+  fetch('https://api.thegraph.com/subgraphs/name/aave/protocol-multy-raw', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query: `
+      query GET_LOANS {
+        users(first:1000, skip:${1000*count}, orderBy: id, orderDirection: desc, where: {borrowedReservesCount_gt: 0}) {
+          id
+          borrowedReservesCount
+          collateralReserve:reserves(where: {principalATokenBalance_gt: 0}) {
+            principalATokenBalance
+            principalBorrows
+            reserve{
+              usageAsCollateralEnabled
+              reserveLiquidationThreshold
+              reserveLiquidationBonus
+              borrowingEnabled
+              utilizationRate
+              symbol
+              underlyingAsset
+              price {
+                priceInEth
+              }
+              decimals
+            }
+          }
+          borrowReserve: reserves(where: {principalBorrows_gt: 0}) {
+            principalATokenBalance
+            principalBorrows
+            reserve{
+              usageAsCollateralEnabled
+              reserveLiquidationThreshold
+              borrowingEnabled
+              utilizationRate
+              symbol
+              underlyingAsset
+              price {
+                priceInEth
+              }
+              decimals
+            }
+          }
+        }
+      }`
+    }),
+  })
+  .then(res => res.json())
+  .then(res => parseUsers(res.data));
+  count++;
+  }
+}
+
+function parseUsers(payload) {
+  console.log(`Records:${payload.users.length} ${Date().toLocaleString()}`)
+  payload.users.forEach((user, i) => {
+    var totalBorrowed=0;
+    var totalCollateral=0;
+    var totalCollateralThreshold=0;
+    var borrowedSymbol;
+    var collateralSymbol;
+    user.borrowReserve.forEach((borrowReserve, i) => {
+      borrowedSymbol = borrowReserve.reserve.symbol
+      var priceInEth= borrowReserve.reserve.price.priceInEth / (10**18)
+      var principalBorrowed = borrowReserve.principalBorrows / (10**borrowReserve.reserve.decimals)
+      totalBorrowed += priceInEth * principalBorrowed
+    });
+    user.collateralReserve.forEach((collateralReserve, i) => {
+      collateralSymbol = collateralReserve.reserve.symbol
+      var priceInEth= collateralReserve.reserve.price.priceInEth / (10**18)
+      var principalATokenBalance = collateralReserve.principalATokenBalance / (10**collateralReserve.reserve.decimals)
+      totalCollateral += priceInEth * principalATokenBalance
+      totalCollateralThreshold += priceInEth * principalATokenBalance * (collateralReserve.reserve.reserveLiquidationThreshold/100)
+    });
+    var healthFactor= totalCollateralThreshold / totalBorrowed;
+    if (healthFactor<=1 && totalCollateral>1)
+      console.log(`user_ID:${user.id} HealthFactor ${healthFactor} totalCollateral ${totalCollateral} ${collateralSymbol}->${borrowedSymbol}` )
+  });
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function updateSwapPrices(){
+  const pair = await Fetcher.fetchPairData(DAI_KOVAN, WETH[DAI_KOVAN.chainId])
+
+  const route = new Route([pair], WETH[DAI_KOVAN.chainId])
+  DAI_WETH = route.midPrice.toSignificant(6)
+  console.log(`${DAI_WETH} DAI per WETH`) // 201.306
+  console.log(route.midPrice.invert().toSignificant(6)) // 0.00496756
+}
